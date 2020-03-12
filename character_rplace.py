652f998934abbf077d0d3016b5116da8c7132e1a
redopm <omprakashmaury001@gmail.com>
2020-03-12 23:22:21 +0530

    mar 12 11:12

---
+++ character_rplace.py
@@ -0,0 +1,23 @@
+s = input()
+num = int(input())
+c= input()
+for i in range(len(s)):
+    if (num == i):
+        print(s[i])
+        s1 =s.replace(s[i], c)
+        #print(i)
+        print(s1)
+
+
+s = input("enter the string : ")
+l = list(s)
+print(l)
+c = input("enter character : ")
+num = int(input("enter numer : "))
+for i in range(l):
+    if num ==i:
+        
+
+
+
+
